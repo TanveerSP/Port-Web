4cb4b5fb93fd85e735f6ff831690668ac182bb5c
Tanveer <tanveerpathan024@gmail.com>
2025-01-30 15:57:51 +0530

    i create faq section and add js for inside faq show img

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+let elemC = document.querySelector("#elem-container");
+let fixedImg = document.querySelector("#fixed-image");
+elemC.addEventListener("mouseenter", function () {
+  fixedImg.style.display = "block";
+});
+elemC.addEventListener("mouseleave", function () {
+  fixedImg.style.display = "none";
+});
+
+let elems = document.querySelectorAll(".elem");
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    let image = e.getAttribute("data-image");
+    fixedImg.style.backgroundImage = `url(${image})`
+    // console.log(image);
+  });
 });
