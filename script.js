24d1b07b076194f46cff70eeb56f952e820c1f66
Tanveer <tanveerpathan024@gmail.com>
2025-02-01 00:03:44 +0530

    add swiper, footer and animation cursor but responsivene is pandding otherwise website is complet

--- script.js
+++ script.js
@@ -20,3 +20,37 @@ elems.forEach(function (e) {
     // console.log(image);
   });
 });
+
+
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+      slidesPerView: "auto",
+      centeredSlides: true,
+      spaceBetween: 100,
+  });
+}
+swiperAnimation()
+
+// Create a custom cursor element
+const cursor = document.createElement('div');
+cursor.id = 'cursor';
+document.body.appendChild(cursor);
+
+// Function to update the position of the custom cursor
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = `${e.pageX}px`;
+  cursor.style.top = `${e.pageY}px`;
+});
+
+// Function to change cursor appearance when hovering over swiper-slide
+document.querySelectorAll('.swiper-slide').forEach(slide => {
+  slide.addEventListener('mouseenter', () => {
+    cursor.style.backgroundColor = '#ff5c0b'; // Change cursor color to orange when hovering over swiper-slide
+    cursor.style.cursor = 'pointer'; // Make the cursor look like a hand
+  });
+
+  slide.addEventListener('mouseleave', () => {
+    cursor.style.backgroundColor = '#ff2d03'; // Reset to initial color when leaving swiper-slide
+    cursor.style.cursor = 'default'; // Revert back to default cursor when not hovering
+  });
+});
